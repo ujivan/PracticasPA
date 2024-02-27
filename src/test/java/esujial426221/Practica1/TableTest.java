d3aca620cbe559b5cbe65328fc34781ba1d2f809
JAVIER <al426144@uji.es>
2024-02-27 18:11:20 +0100

    Test borrador

---
+++ src/test/java/esujial426221/Practica1/TableTest.java
@@ -0,0 +1,15 @@
+package esujial426221.Practica1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TableTest {
+
+    @Test
+    @DisplayName("TestNumeroColumnas")
+    void getRowAt() {
+
+    }
+}
